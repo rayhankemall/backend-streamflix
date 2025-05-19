610d418a61f4fcb4c1952d6341bbf100bda4589a
rayhankemall <rayhankemallpasha@gmail.com>
2025-05-19 16:01:33 +0700

    Fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
@@ -13,6 +13,17 @@ export class UsersService {
   ) {}
 
   async createUser(dto: CreateUserDto): Promise<User> {
+    // 🔍 Cek apakah email sudah ada
+    const existingUser = await this.userRepo.findOne({
+      where: { email: dto.email },
+    });
+
+    if (existingUser) {
+      // ❌ Jika sudah ada, kirim error
+      throw new ConflictException('Email sudah digunakan');
+    }
+
+    // 🔐 Hash password dan simpan
     const hash = await bcrypt.hash(dto.password, 10);
     const newUser = this.userRepo.create({ ...dto, password: hash });
     return this.userRepo.save(newUser);
